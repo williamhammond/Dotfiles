1bdc3cf3ddd802ed58c44722a3e407c2ce155317
WilliamHammond <william.t.hammond@gmail.com>
2023-12-17 23:49:47 -0500

    Add some better keyboard shortcuts

--- install-mac.sh
+++ install-mac.sh
@@ -14,3 +14,14 @@ git config --global user.name "WilliamHammond"
 if [[ ! -f "$HOME/.ssh/id_ed25519" ]]; then
     ssh-keygen -t ed25519 -a 100 </dev/null
 fi
+
+# Set fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
+
+# enable keyboard navigation to move focus between controls (tab / shift-tab
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# default behavior when holding down a key is to bring up a menu of characters with different diacritical marks.
+# Try holding down ‘e’ to see this in action. If you want to instead repeat characters when a key is held:
+defaults write -g ApplePressAndHoldEnabled -bool false
