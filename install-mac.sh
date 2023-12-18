5afda8e1e8c89a30376665c31fbb8f44e9ca9091
WilliamHammond <william.t.hammond@gmail.com>
2023-12-17 23:48:13 -0500

    Update brew if installed

--- install-mac.sh
+++ install-mac.sh
@@ -1,11 +1,16 @@
 set -euo
 
-if ! command -v brew &> /dev/null
-then
+if ! command -v brew &> /dev/null; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    brew update && brew upgrade
 fi
 
 brew bundle
 
 git config --global user.email "william.t.hammond@gmail.com"
 git config --global user.name "WilliamHammond"
+
+if [[ ! -f "$HOME/.ssh/id_ed25519" ]]; then
+    ssh-keygen -t ed25519 -a 100 </dev/null
+fi
