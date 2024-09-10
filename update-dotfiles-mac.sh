d3a30eb58816d78b73f23e49b63819f03ddb0a6c
William Hammond <william.t.hammond@gmail.com>
2024-09-10 17:03:44 -0400

    Disable visual bell

--- update-dotfiles-mac.sh
+++ update-dotfiles-mac.sh
@@ -3,5 +3,6 @@ set -euo pipefail
 cp .vimrc ~
 cp .ideavimrc ~
 cp .zshrc ~
+copy .inputrc ~
 mkdir -p ~/.config/nvim
 cp nvim/init.lua ~/.config/nvim/init.lua
--- update-dotfiles.bat
+++ update-dotfiles.bat
@@ -4,5 +4,6 @@
 copy .vimrc %USERPROFILE%
 copy .ideavimrc %USERPROFILE%
 copy .zshrc %USERPROFILE%
+copy .inputrc %USERPROFILE%
 if not exist "%localappdata%\nvim" mkdir "%localappdata%\nvim"
 copy nvim\init.lua "%localappdata%\nvim\init.lua"
