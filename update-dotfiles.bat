e2d474849a9e0e6511986d4f22b8a0d44bc92652
William Hammond <william.t.hammond@gmail.com>
2024-09-10 17:12:03 -0400

    Do ctrl+backspace correctly

--- update-dotfiles.bat
+++ update-dotfiles.bat
@@ -5,5 +5,6 @@ copy .vimrc %USERPROFILE%
 copy .ideavimrc %USERPROFILE%
 copy .zshrc %USERPROFILE%
 copy .inputrc %USERPROFILE%
+copy settings.json %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
 if not exist "%localappdata%\nvim" mkdir "%localappdata%\nvim"
 copy nvim\init.lua "%localappdata%\nvim\init.lua"
